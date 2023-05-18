c58b9a79359cb9915433b23331911b22fe7f02a1
Pastakun <130649332+Pastakun@users.noreply.github.com>
2023-05-18 14:38:53 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,44 +1,49 @@
 class Test {
-
-  constructor() {}
-
-  getInfo() {
-    return {
-      id: 'test',
-      name: 'Test',
-      blocks: [
-        {
-        	opcode: 'cloudvalue', 
-        	blockType: Scratch.BlockType.REPORTER,
-        	text: '☁ [name]',
-        	arguments: {
-        		name: {
-        			type: Scratch.ArgumentType.STRING,
-        			defaultValue: ''
-        		}
-        	}
-        },
-        {
-        	opcode: 'projectid', 
-        	blockType: Scratch.BlockType.COMMAND,
-        	text: 'プロジェクト[projectid]に接続する',
-        	arguments: {
-        		projectid: {
-        			type: Scratch.ArgumentType.NUMBER,
-        			defaultValue: ''
-        		}
-        	}
-        }
-      ]
-    }
-  }
-
-  cloudvalue(args) {
-    return "パスタくんえらい！";
-  }
-  projectid(args) {
-    console.log("test");
-  }
+	const user = "player";
+	let projectid = "";
+	const socket = new WebSocket('wss://clouddata.turbowarp.org/');
+	function cloudsend(method,user,project_id,name,value) {
+		socket.send("".concat(JSON.stringify({"method":method,"user":user,"project_id":project_id,"name":name,"value":value}),"\n"));
+	}
+	constructor() {}
+	getInfo() {
+		return {
+			id: 'test',
+			name: 'Test',
+			blocks: [
+				{
+					opcode: 'cloudvalue', 
+					blockType: Scratch.BlockType.REPORTER,
+					text: '☁ [name]',
+					arguments: {
+						name: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: ''
+						}
+					}
+				},
+				{
+					opcode: 'projectid', 
+					blockType: Scratch.BlockType.COMMAND,
+					text: 'プロジェクト[projectid]に接続する',
+					arguments: {
+						projectid: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: ''
+						}
+					}
+				}
+			]
+		}
+	}
+	
+	cloudvalue(args) {
+		return "パスタくんえらい！";
+	}
+	projectid(args) {
+		projectid = args.projectid
+		cloudsend("handshake",user,projectid);
+	}
 }
 
 Scratch.extensions.register(new Test());
