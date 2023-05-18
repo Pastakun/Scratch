1024be9c287fd525bef66352bbd5538cc9a51d05
Pastakun <130649332+Pastakun@users.noreply.github.com>
2023-05-18 17:43:27 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,20 @@
+const projectidlist = [];
+function cloud(projectid) {
+	if (projectidlist.indexOf(projectid) === -1) {
+		projectidlist.push(projectid);
+		const socket = new WebSocket('wss://clouddata.turbowarp.org/');
+		
+		function cloudsend(method,user,project_id,name,value) {
+			socket.send("".concat(JSON.stringify({"method":method,"user":user,"project_id":project_id,"name":name,"value":value}),"\n"));
+		}
+		socket.addEventListener('open', function (event) {
+			cloudsend("handshake","player",projectid);
+		});
+		socket.addEventListener('message', function (event) {
+			console.log(event.data.split("\n"));
+		});
+	}
+}
 class Test {
 //constructor() {}
 	getInfo() {
@@ -8,9 +25,17 @@ class Test {
 				{
 					opcode: 'cloudvalue', 
 					blockType: Scratch.BlockType.REPORTER,
-					text: '☁ [name]',
+					text: 'プロジェクトid [projectid] の☁ [name] を [value] にする',
 					arguments: {
+						projectid: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: ''
+						}
 						name: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: ''
+						}
+						value: {
 							type: Scratch.ArgumentType.STRING,
 							defaultValue: ''
 						}
@@ -19,10 +44,14 @@ class Test {
 				{
 					opcode: 'projectid', 
 					blockType: Scratch.BlockType.COMMAND,
-					text: 'プロジェクト[projectid]に接続する',
+					text: 'プロジェクトid [projectid] の☁ [name] ',
 					arguments: {
 						projectid: {
 							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: ''
+						}
+						name: {
+							type: Scratch.ArgumentType.STRING,
 							defaultValue: ''
 						}
 					}
@@ -32,20 +61,11 @@ class Test {
 	}
 	
 	cloudvalue(args) {
+		cloud(args.projectid);
 		return "パスタくんえらい！";
 	}
 	projectid(args) {
-		const socket = new WebSocket('wss://clouddata.turbowarp.org/');
-		
-		function cloudsend(method,user,project_id,name,value) {
-			socket.send("".concat(JSON.stringify({"method":method,"user":user,"project_id":project_id,"name":name,"value":value}),"\n"));
-		}
-		socket.addEventListener('open', function (event) {
-			cloudsend("handshake","player",args.projectid);
-		});
-		socket.addEventListener('message', function (event) {
-			console.log(event.data.split("\n"));
-		});
+		cloud(args.projectid);
 	}
 }
 
