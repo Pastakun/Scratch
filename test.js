a73f6d46e268967d89e9686009022571916a354c
Pastakun <130649332+Pastakun@users.noreply.github.com>
2023-05-19 16:46:10 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,60 +1,64 @@
-let projectidlist = [];
+let connectionprojectid = "";
+let newprojectid = "";
 let cloudnamelist = [];
 let cloudvaluelist = [];
 let cloudsetvaluelist = [];
 
 function cloud(projectid) {
-	if (projectidlist.indexOf(projectid) === -1) {
-		let close = false;
-		projectidlist.push(projectid);
-		cloudnamelist.push([]);
-		cloudvaluelist.push([]);
-		cloudsetvaluelist.push([]);
-		const projectidlistnumber = projectidlist.indexOf(projectid);
-		const socket = new WebSocket('wss://clouddata.turbowarp.org/');
-		
-		function cloudsend(method,user,project_id,name,value) {
-			socket.send("".concat(JSON.stringify({"method":method,"user":user,"project_id":project_id,"name":name,"value":value}),"\n"));
+	let close = false;
+	cloudnamelist.push([]);
+	cloudvaluelist.push([]);
+	cloudsetvaluelist.push([]);
+	const socket = new WebSocket('wss://clouddata.turbowarp.org/');
+	
+	function cloudsend(method,user,project_id,name,value) {
+		socket.send("".concat(JSON.stringify({"method":method,"user":user,"project_id":project_id,"name":name,"value":value}),"\n"));
+	}
+	socket.addEventListener('open', function (event) {
+		cloudsend("handshake","player",projectid);
+		let socketopen = window.setInterval(cloudset, 100);
+		function cloudset() {
+			if (close) {
+				window.clearInterval(socketopen);
+			}else{
+				if (cloudsetvaluelist.length !== 0) {
+					const cloudsetvalue = cloudsetvaluelist[Math.floor(Math.random()*cloudsetvaluelist.length)];
+					cloudsend("set", "player", projectid, cloudsetvalue.name, cloudsetvalue.value);
+					cloudsetvaluelist = [];
+				}
+			}
 		}
-		socket.addEventListener('open', function (event) {
-			cloudsend("handshake","player",projectid);
-			let socketopen = window.setInterval(cloudset, 150);
-			function cloudset() {
-				if (close) {
-					window.clearInterval(socketopen);
-				}else{
-					if (cloudsetvaluelist[projectidlistnumber].length !== 0) {
-						const cloudsetvalue = cloudsetvaluelist[projectidlistnumber][Math.floor(Math.random()*cloudsetvaluelist[projectidlistnumber].length)];
-						cloudsend("set", "player", projectid, cloudsetvalue.name, cloudsetvalue.value);
-						cloudsetvaluelist[projectidlistnumber].splice(0);
-					}
+	});
+	socket.addEventListener('message', function (event) {
+		const clouddatalist = event.data.split("\n");
+		for (let i = 0; i < clouddatalist.length; i++){
+			const clouddata = JSON.parse(clouddatalist[i]);
+			if (clouddata.method === "set") {
+				if (cloudnamelist.indexOf(clouddata.name) === -1 ) {
+					cloudnamelist.push(clouddata.name);
+					cloudvaluelist.push("");
 				}
+				cloudvaluelist[cloudnamelist.indexOf(clouddata.name)] = clouddata.value;
 			}
-		});
-		socket.addEventListener('message', function (event) {
-			const clouddatalist = event.data.split("\n");
-			for (let i = 0; i < clouddatalist.length; i++){
-				const clouddata = JSON.parse(clouddatalist[i]);
-				if (clouddata.method === "set") {
-					if (cloudnamelist[projectidlistnumber].indexOf(clouddata.name) === -1 ) {
-						cloudnamelist[projectidlistnumber].push(clouddata.name);
-						cloudvaluelist[projectidlistnumber].push("");
-					}
-					const cloudnamelistnumber = cloudnamelist[projectidlistnumber].indexOf(clouddata.name);
-					cloudvaluelist[projectidlistnumber][cloudnamelistnumber] = clouddata.value;
-				}
-			}
-		});
-		socket.addEventListener('close', function (event) {
-			close = true;
-		    setTimeout(() => {
-		    	projectidlist[projectidlistnumber] = "";
+		}
+	});
+	socket.addEventListener('close', function (event) {
+		close = true;
+			setTimeout(() => {
 				cloud(projectid);
-    		}, 3000);
+			}, 3000);
 		}); 
-		
 	}
 }
+
+let socketopen = window.setInterval(cloudset, 0);
+function cloudset() {
+	if (connectionprojectid !== newprojectid) {
+		cloud(newprojectid);
+		connectionprojectid = newprojectid
+	}
+}
+
 class Test {
 //constructor() {}
 	getInfo() {
@@ -62,6 +66,17 @@ class Test {
 			id: 'test',
 			name: 'Test',
 			blocks: [
+				{
+					opcode: 'projectid', 
+					blockType: Scratch.BlockType.COMMAND,
+					text: 'プロジェクトid [projectid]',
+					arguments: {
+						projectid: {
+							type: Scratch.ArgumentType.NUMBER,
+							defaultValue: ''
+						}
+					}
+				},
 				{
 					opcode: 'setcloud', 
 					blockType: Scratch.BlockType.COMMAND,
@@ -84,7 +99,7 @@ class Test {
 				{
 					opcode: 'cloudvalue', 
 					blockType: Scratch.BlockType.REPORTER,
-					text: 'プロジェクトid [projectid] の☁ [name] ',
+					text: '[projectid] の☁ [name] ',
 					arguments: {
 						projectid: {
 							type: Scratch.ArgumentType.NUMBER,
@@ -100,16 +115,17 @@ class Test {
 		}
 	}
 	
+	projectid(args) {
+		newprojectid = args.projectid
+	}
 	setcloud(args) {
-		cloud(args.projectid);
-		cloudsetvaluelist[projectidlist.indexOf(args.projectid)].push({name: "☁ " + args.name, value: args.value});
+		cloudsetvaluelist.push({name: "☁ " + args.name, value: args.value});
 		//console.log(projectidlist);
 		//console.log(cloudnamelist);
 		//console.log(cloudvaluelist);
 	}
 	cloudvalue(args) {
-		cloud(args.projectid);
-		return cloudvaluelist[projectidlist.indexOf(args.projectid)][cloudnamelist[projectidlist.indexOf(args.projectid)].indexOf("☁ " + args.name)];
+		return cloudvaluelist[cloudnamelist.indexOf("☁ " + args.name)];
 	}
 }
 
